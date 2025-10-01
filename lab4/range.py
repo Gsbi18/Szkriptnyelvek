f19136d2666460c8afce5de29b39e6ff0fc9bde6
Gábor Áncsán <74606522+Gsbi18@users.noreply.github.com>
2025-10-01 09:32:22 +0200

    4.oravege

--- lab4/range.py
+++ lab4/range.py
@@ -2,11 +2,11 @@
 
 
 def main():
-    print("Hello")
-
-
-##################################################
-
+    a = list(range(23,47))
+    b = list(range(53,26,-1))
+    
+    print(b)
+    
 
 if __name__ == "__main__":
     main()
